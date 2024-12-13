91c7286864e4874900f83d67b8b0c8b47398bad9
James Christian Wira <jamesch888@gmail.com>
2024-12-13 12:55:21 +0700

    feat(-/JCW): create custom search bar

---
+++ lib/features/search/custom_search_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({
+    super.key,
+    required this.onChanged,
+    required this.icon,
+    required this.hintText,
+  });
+
+  final Function(String) onChanged;
+  final Icon icon;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        prefixIcon: icon,
+        hintText: hintText,
+      ),
+      onChanged: (text) {
+        onChanged(text);
+      },
+    );
+  }
+}
